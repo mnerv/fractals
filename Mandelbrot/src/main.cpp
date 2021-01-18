b31baac89e94f3bab982b9e8e994dc1c9c1208ee
KTheXIII <pratchaya.k.git@gmail.com>
2021-01-18 06:01:35 +0100

    Added multithreaded support for CPU

--- Mandelbrot/src/main.cpp
+++ Mandelbrot/src/main.cpp
@@ -5,18 +5,19 @@
 #include <vector>
 #include <string.h>
 #include <cmath>
+#include <thread>
 
 #include "stb/stb_image_write.h"
 
-constexpr int WIDTH = 512;
-constexpr int HEIGHT = 512;
-constexpr int MAX_ITERATIONS = 512;
+constexpr int WIDTH = 8192;
+constexpr int HEIGHT = 8192;
+constexpr int MAX_ITERATIONS = 1024;
 
 template <typename T, typename = typename std::enable_if<
                           std::is_floating_point<T>::value, T>::type>
 int mandelbrot(T x, T y, int max, T scale = 1.0, T offset_x = 0.0,
-               T offset_y = 0) {
-    T r2 = 2 * 2;
+               T offset_y = 0, T radius = 2.0) {
+    T r2 = radius * radius;
 
     T zr = 0;
     T zr_tmp = 0;
@@ -33,8 +34,7 @@ int mandelbrot(T x, T y, int max, T scale = 1.0, T offset_x = 0.0,
         zi = 2.0 * zr * zi + ci;
         zr = zr_tmp;
 
-        if (zr * zr + zi * zi > r2)
-            break;
+        if (zr * zr + zi * zi > r2) break;
 
         iterations++;
     }
@@ -54,24 +54,70 @@ T map(T x, T in_min, T in_max, T out_min, T out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+// clang-format off
+const std::string TITLE = 
+    "-----------------------------------------------------\n"
+    "|                 Mandelbrot set                    |\n"
+    "-----------------------------------------------------\n";
+// clang-format on
+
 int main(int argc, char const* argv[]) {
     const int CHANNELS = 4;
     const int max_iterations = MAX_ITERATIONS;
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed;
+    auto start = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Hello Mandelbrot set!\n";
+    volatile bool is_done = false;
+    std::cout << TITLE;
 
+    std::cout << "Allocating " << WIDTH * HEIGHT * CHANNELS * sizeof(uint8_t)
+              << " bytes of memory for image..." << std::endl;
+    start = std::chrono::high_resolution_clock::now();
     uint8_t* pixels = new uint8_t[WIDTH * HEIGHT * CHANNELS];
+    std::cout << "Time took: " << elapsed.count() << "s\n\n";
+
+    std::cout << "Allocating " << WIDTH * HEIGHT * sizeof(int32_t)
+              << " bytes of memory for Mandelbrot...\n";
+    start = std::chrono::high_resolution_clock::now();
     int* fractals = new int[WIDTH * HEIGHT];
+    elapsed = std::chrono::high_resolution_clock::now() - start;
+    std::cout << "Time took: " << elapsed.count() << "s\n\n";
 
-    for (int i = 0; i < HEIGHT; i++) {
-        double y = map<double>(i + 1, 1.0, HEIGHT, 1.0, -1.0);
-        for (int j = 0; j < WIDTH; j++) {
-            double x = map<double>(j + 1, 1.0, HEIGHT, -1.0, 1.0);
-
-            fractals[i * WIDTH + j] =
-                mandelbrot<double>(x, y, max_iterations, 1.2);
+    auto do_work = [&](int row_start, int row_end) {
+        for (int i = row_start; i < row_end; i++) {
+            double y = map<double>(i + 1, 1.0, HEIGHT, 1.0, -1.0);
+            for (int j = 0; j < WIDTH; j++) {
+                double x = map<double>(j + 1, 1.0, HEIGHT, -1.0, 1.0);
+                fractals[i * WIDTH + j] =
+                    mandelbrot<double>(x, y, max_iterations, 1.2);
+            }
         }
+    };
+
+    const uint32_t n_threads = std::thread::hardware_concurrency();
+    std::cout << "Hardware thread count: " << n_threads << "\n";
+    const int thread_count = n_threads;  // 32;
+    std::cout << "Thread count: " << thread_count << "\n";
+    std::thread* t = new std::thread[thread_count];
+
+    const int row_offset = HEIGHT / thread_count;
+
+    std::cout << "Generating fractals...\n";
+    start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < thread_count; i++) {
+        t[i] = std::thread(do_work, row_offset * i, row_offset * (i + 1));
     }
+
+    for (int i = 0; i < thread_count; i++) { t[i].join(); }
+
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = end - start;
+    std::cout << "Time took: " << elapsed.count() << " s\n\n";
+
+    std::cout << "Generating image...\n";
+    start = std::chrono::high_resolution_clock::now();
 
     int index = 0;
     for (int i = 0; i < HEIGHT; i++) {
@@ -101,11 +147,22 @@ int main(int argc, char const* argv[]) {
             }
         }
     }
+    elapsed = std::chrono::high_resolution_clock::now() - start;
+    std::cout << "Time took: " << elapsed.count() << "s\n\n";
 
-    stbi_write_png("mandelbrot.png", WIDTH, HEIGHT, CHANNELS, pixels,
-                   WIDTH * CHANNELS);
+    // std::cout << "Writing image to disc...\n";
+    // start = std::chrono::high_resolution_clock::now();
+    // stbi_write_png("mandelbrot.png", WIDTH, HEIGHT, CHANNELS, pixels,
+    //                WIDTH * CHANNELS);
+    // elapsed = std::chrono::high_resolution_clock::now() - start;
+    // std::cout << "Time took: " << elapsed.count() << "s\n\n";
+
+    std::cout << "Clean up...\n";
     delete[] pixels;
     delete[] fractals;
+
+    std::cout << "Press enter to exit...";
+    std::cin.get();
 
     return 0;
 }
